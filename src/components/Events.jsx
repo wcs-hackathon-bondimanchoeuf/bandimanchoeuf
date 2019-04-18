f925616cc9d08f999aaaaf0ee249d4dc167073d7
Florian Gardy <florian.gardy@gmail.com>
2019-04-18 17:32:52 +0200

    Added title and table in Events component

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Table } from "reactstrap";
 
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div>
+      <h2>Last events</h2>
+      <Table responsive>
+        <tbody>
+          <tr>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default Events;
