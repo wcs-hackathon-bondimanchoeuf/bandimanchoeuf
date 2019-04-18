3af77237167d83ff096aaa1bcc473af4a08e46f1
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-18 17:34:16 +0200

    adding container for legend comoonent

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Col, Row } from "reactstrap";
 
 const Legend = () => {
-  return <div>Legend</div>;
+  return (
+    <div>
+      <Row>
+        <Col xs="4">
+          <div>XXX - 10 points</div>
+        </Col>{" "}
+        <Col xs="4">
+          <div>XXX - 10 points</div>
+        </Col>{" "}
+        <Col xs="4">
+          <div>XXX - 10 points</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs="4">
+          <div>XXX - 10 points</div>
+        </Col>{" "}
+        <Col xs="4">
+          <div>XXX - 10 points</div>
+        </Col>{" "}
+        <Col xs="4">
+          <div>XXX - 10 points</div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Legend;
