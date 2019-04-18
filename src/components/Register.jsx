e837aa4193a5fa793db4d612ad6e6516199747c5
BB5014 <bblabriz@bbox.fr>
2019-04-18 19:56:51 +0200

    added form in Register component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
-const Register = () => {
-  return <div>Register</div>;
-};
+class Register extends React.Component {
+  render() {
+    return (
+      <Form>
+        <h2>New Game</h2>
+        <FormGroup>
+          <Label for="fieldname">Your name</Label>
+          <Input type="text" name="name" id="fieldname" placeholder="Name" />
+        </FormGroup>
+
+        <Button>Start</Button>
+      </Form>
+    );
+  }
+}
 
 export default Register;
