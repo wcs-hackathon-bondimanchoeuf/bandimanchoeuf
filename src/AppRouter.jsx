7685241fb05db4d371aa6259fd1f44234bf98a32
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-18 14:50:27 +0200

    Adding Components Header Home Footer Game

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./components/Home.jsx";
+import Game from "./components/Game.jsx";
 
 const AppRouter = () => {
-  return <div>My router</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Game" component={Game} />
+      </Switch>
+
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
