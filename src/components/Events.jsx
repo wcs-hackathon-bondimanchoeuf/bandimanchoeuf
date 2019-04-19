8b6c5aac1287b6eeacee82bcb7682a9f7a90e6d4
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-19 13:40:44 +0200

    adding last events and changing title colors

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,36 +2,62 @@ import React from "react";
 import { Table } from "reactstrap";
 import style from "./Events.module.css";
 
-const Events = () => {
+const Events = props => {
+  console.log("events props");
+  console.log(props);
+
+  let tbody = [];
+  tbody = props.events
+    .map(event => (
+      <tr>
+        <td>
+          <img
+            src={props.eggs[event[0]].image}
+            style={{ width: "18px", height: "18px" }}
+          />
+          <img
+            src={props.eggs[event[1]].image}
+            style={{ width: "18px", height: "18px" }}
+          />
+          <img
+            src={props.eggs[event[2]].image}
+            style={{ width: "18px", height: "18px" }}
+          />
+        </td>
+        <td> Score : {event[3]}</td>
+      </tr>
+    ))
+    .reverse()
+    .filter((x, i) => i <= 4);
+
   return (
     <div className={style.block}>
-      <h2 style={{ color: "#17a2b8" }}>Last events</h2>
-      <Table responsive size="sm" hover>
+      <h2 className="text-success">Last events</h2>
+      <table
+        size="sm"
+        hover
+        style={{
+          display: "flex",
+          flexDirection: "column-reverse ",
+          marginLeft: "25%"
+        }}
+      >
         <tbody>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
+          {tbody}
+          {/* <EventCell {...props} /> */}
         </tbody>
-      </Table>
+      </table>
     </div>
   );
 };
 
 export default Events;
+
+// return (
+//   <tr>
+//     <td>
+//       <img src={props.eggs[0]} style={{ width: '18px' }} alt='An Egg' />
+//     </td>
+//     <td>Table cell</td>
+//   </tr>
+// );
